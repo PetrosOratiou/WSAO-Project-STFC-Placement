b4787e0ebb599a393db692c4c3f4274ee84e99ff
Petros Oratiou <67263427+PetrosOratiou@users.noreply.github.com>
2020-07-01 13:50:35 +0100

    Update rand_coeffs.m

--- DeepLearning/rand_coeffs.m
+++ DeepLearning/rand_coeffs.m
@@ -1,7 +1,7 @@
 function coeffs = rand_coeffs(ords, decay)
     N = sqrt(ords);
     if N ~= round(N)
-        error('N must be the square of a natural number. eg. 16,25,32,...');
+        error('N must be the square of a natural number. eg. 16,25,36,...');
     end
     [weightsR, weightsC] = meshgrid(1:N);
     weights = weightsR.^2 + weightsC.^2;
