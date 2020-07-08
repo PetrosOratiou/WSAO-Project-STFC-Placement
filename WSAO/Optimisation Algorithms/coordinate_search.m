b322b2b39e53f22ee2b050cb23f8b3013d7f5786
Petros Oratiou <67263427+PetrosOratiou@users.noreply.github.com>
2020-07-08 18:25:27 +0100

    Updated cs and added usage example

--- WSAO/Optimisation Algorithms/coordinate_search.m
+++ WSAO/Optimisation Algorithms/coordinate_search.m
@@ -1,71 +1,54 @@
-classdef coordinate_search < handle
-  properties
-    position
-    next_position
-    number_dimensions
-    number_samples
-    cost_function
-    lower_limits
-    upper_limits
-    position_cost = inf;
-    eval_function
-    samp
-  end
-  methods
-    function obj = coordinate_search(dims,costfun,numSamples)
-      obj.number_dimensions = dims;
-      obj.number_samples = numSamples;
-      obj.cost_function = costfun;
-      obj.position  = zeros(dims,1);
-      obj.next_position = zeros(dims,1);
-      obj.eval_function = @(i)numSamples*dims*i;
-      reset_solver(obj);
-      obj.samp = cell(obj.number_samples,obj.number_dimensions);
+classdef coordinate_search < Solver
+    properties
+        sMatrix
+        srange
+        origin
+        samples = 5;
     end
-    function step(obj)
-      for i = 1:obj.number_dimensions
-        sample_coordinates = linspace(obj.lower_limits(i),obj.upper_limits(i),obj.number_samples);
-        cost = nan(obj.number_samples,1);
-        for k = 1:obj.number_samples
-          temp = obj.position;
-          temp(i) = sample_coordinates(k);
-          cost(k) = obj.cost_function(temp);
-          obj.samp{k,i} = temp;
+    
+    methods
+        function self = coordinate_search(dim,fhandle,varargin)
+            if nargin == 3
+                self.samples = varargin{1};
+            end
+            self.dimensions = dim;
+            self.cost_function = fhandle;
+            self.reset();
         end
-        [obj.position_cost,k] = min(cost);
-        obj.next_position(i) = sample_coordinates(k);
-        if k ~= 1 && k ~= obj.number_samples
-          obj.lower_limits(i) = sample_coordinates(k-1);
-          obj.upper_limits(i) = sample_coordinates(k+1);
-        else
-          offset = obj.next_position(i) - obj.position(i);
-          if offset + obj.lower_limits(i) < -1
-            obj.lower_limits(i) = -1;
-            obj.upper_limits(i) = obj.upper_limits(i) + offset;
-          elseif offset + obj.upper_limits(i) > 1
-            obj.upper_limits(i) = 1;
-            obj.lower_limits(i) = obj.lower_limits(i) + offset;
-          else
-            obj.lower_limits(i) = obj.lower_limits(i) + offset;
-            obj.upper_limits(i) = obj.upper_limits(i) + offset;
-          end
+        function reset(self,varargin)
+            % Optional argument specifies initial position
+            self.srange = [-ones(1,self.dimensions);ones(1,self.dimensions)];
+            self.cost = inf(self.samples,1);
+            if nargin == 2
+                self.position = varargin{1};
+            else
+                self.position = zeros(self.dimensions,1);
+            end
+            self.origin = self.position;
         end
-        obj.position(i) = obj.next_position(i);
-      end
+        function step(self)
+            for i = 1:self.dimensions
+                xvals = linspace(self.srange(1,i),self.srange(2,i),self.samples);
+                self.sMatrix = self.origin + zeros(self.dimensions,self.samples);
+                mincost = inf;
+                for j = 1:self.samples
+                    self.sMatrix(i,j) = xvals(j);
+                    self.cost(j) = self.cost_function(self.sMatrix(:,j));
+                    self.evaluations = self.evaluations + 1;
+                    if self.cost(j) <= mincost
+                        mincost = self.cost(j);
+                        idx = j;
+                    end
+                end
+                self.position = self.sMatrix(:,idx);
+                if idx == 1 || idx == self.samples
+                    self.srange = self.srange + (self.position' - self.origin');
+                else
+                    self.srange(1,i) = self.sMatrix(i,idx-1);
+                    self.srange(2,i) = self.sMatrix(i,idx+1);
+                end
+                self.origin = self.position;
+            end
+        end
     end
-    function reset_solver(obj,varargin)
-      if nargin == 1
-        lower_limit_value = -1;
-        upper_limit_value = 1;
-      else
-        lower_limit_value = varargin{1};
-        upper_limit_value =  varargin{2};
-      end
-        obj.lower_limits = lower_limit_value.*ones(obj.number_dimensions,1);
-        obj.upper_limits = upper_limit_value.*ones(obj.number_dimensions,1);
-        obj.position  = zeros(obj.number_dimensions,1);
-        obj.next_position = zeros(obj.number_dimensions,1);
-        obj.position_cost = inf;
-    end
-  end
 end
