b833bb2f66e16b850f6b98e6e528489d6548fb9f
Petros Oratiou <67263427+PetrosOratiou@users.noreply.github.com>
2020-07-10 09:33:29 +0100

    Updated solvers

--- WSAO/Optimisation Algorithms/gdm_solver.m
+++ WSAO/Optimisation Algorithms/gdm_solver.m
@@ -1,9 +1,6 @@
-classdef gdm_solver < handle
+classdef gdm_solver < Solver
+    % Gradient descent with momentum. Inherits from solver
     properties
-        numdims
-        costfun
-        cost
-        position
         momentum = 0.5
         gradient = inf
         convrate = 0.1
@@ -14,44 +11,49 @@ classdef gdm_solver < handle
     end
     methods
         function obj = gdm_solver(dims,costfun)
-            obj.numdims = dims;
-            obj.costfun = @(r)costfun(r) + obj.L2*sum(r.^2,'all');
-            obj.position = zeros(dims,1);
+            obj.dimensions = dims;
+            obj.cost_function = @(r)costfun(r) + obj.L2*sum(r.^2,'all');
+            obj.reset();
         end
-        function step(obj)
-            obj.cost = obj.costfun(obj.position);
-            pos2 = obj.gradstep*eye(obj.numdims) + obj.position;
-            cost2 = zeros(obj.numdims,1);
-            for i = 1:obj.numdims
-                cost2(i) = obj.costfun(pos2(:,i))';
+        function pos = step(obj)
+            obj.cost = obj.cost_function(obj.position);
+            obj.evaluations = obj.evaluations + 1;
+            pos2 = obj.gradstep*eye(obj.dimensions) + obj.position;
+            cost2 = zeros(obj.dimensions,1);
+            for i = 1:obj.dimensions
+                cost2(i) = obj.cost_function(pos2(:,i))';
+                obj.evaluations = obj.evaluations + 1;
             end
             obj.gradient = (cost2-obj.cost)/obj.gradstep;
             obj.weightedgrad = obj.momentum*obj.weightedgrad +...
                 (1-obj.momentum)*obj.gradient;
             obj.position = obj.position - obj.convrate*obj.weightedgrad;
+            pos = obj.position;
         end
         function settings(obj,varargin)
             % Leave argument as [] if you don't want to change it
-            % Read object properties
             prop_array = [
                 obj.convrate;
                 obj.momentum;
                 obj.gradstep;
                 obj.L2;
                 ];
-            % Check for non-empty elements. Non-empty: idx = 1. Else 0
             idx = ~cellfun(@isempty,varargin);
             for i = 1:nargin-1
-                % If not empty, overwrite
                 if idx(i)
                     prop_array(i) = varargin{i};
                 end
             end
-            % Change properties
             obj.convrate = prop_array(1);
             obj.momentum = prop_array(2);
             obj.gradstep = prop_array(3);
             obj.L2       = prop_array(4);
+        end
+        function reset(obj)
+            obj.position = zeros(obj.dimensions,1);
+            obj.cost = inf;
+            obj.weightedgrad = 0;
+            obj.evaluations = 0;
         end
     end
 end
--- WSAO/Optimisation Algorithms/simplex_optimiser.m
+++ WSAO/Optimisation Algorithms/simplex_optimiser.m
@@ -1,46 +1,45 @@
-classdef simplex_optimiser < handle
+classdef simplex_optimiser < Solver
     properties
-        dimensions
         simplex
-        position
-        cost_function
         simplex_cost
-        init_size = 0.1;
-
+        init_size = 0.25;
+        
         r_coeff = 1
         e_coeff = 2
-        c_coeff = .5
-        s_coeff = .5
+        c_coeff = 0.5
+        s_coeff = 0.5
     end
     methods
+        
         function self = simplex_optimiser(dims,costfun)
             self.dimensions = dims;
             self.cost_function = costfun;
-            self.initialise_simplex();
+            self.reset();
         end
-        function initialise_simplex(self)
+        
+        function reset(self)
             N = self.dimensions;
             init_position = zeros(N,1);
             nms = self.init_size*eye(N) + init_position;
             nms = cat(2,nms,init_position);
             self.simplex = nms;
             self.position = init_position;
+            self.evaluations = 0;
         end
+        
         function step(self,varargin)
-            
+            % See wikipedia on nelder mead simplex method for algorithm
             steps = 1;
             if ~isempty(varargin)
                 steps = varargin{1};
             end
             
             for i = 1:steps
-                
                 self.sort_simplex();
                 sec_to_worst_cost = self.simplex_cost(end-1);
                 best_cost = self.simplex_cost(1);
 
                 centroid = self.get_centroid();
-                % centroid_cost = self.evaluate(centroid);
 
                 reflected = self.get_reflected(centroid);
                 reflected_cost = self.evaluate(reflected);
@@ -71,12 +70,14 @@ classdef simplex_optimiser < handle
                 self.position = self.simplex(:,1);
             end
         end
+        
         function sort_simplex(self)
             % Sorting in ascending order: lower cost first (best < worst)
             self.simplex_cost = self.evaluate(self.simplex);
             [self.simplex_cost,idx] = sort(self.simplex_cost);
             self.simplex = self.simplex(:,idx);
         end
+        
         function cost = evaluate(self,points)
             % Evaluates a single point or set of points
             % Must be N by M matrix where M is number of points
@@ -84,22 +85,32 @@ classdef simplex_optimiser < handle
             cost = zeros(1,num_points);
             for idx = 1:num_points
                 cost(idx) = self.cost_function(points(:,idx));
+                self.evaluations = self.evaluations + 1;
             end
         end
+        
         function centroid = get_centroid(self)
+            % Centroid of all points besides worst
             centroid = mean(self.simplex(:,1:end-1),2);
         end
+        
         function reflected = get_reflected(self,centroid)
+            % Reflection of worst from the centroid
             worst = self.simplex(:,end);
             reflected = centroid + self.r_coeff*(centroid-worst);
         end
+        
         function expanded = get_expanded(self,centroid,reflected)
+            % Expanding reflected point
             expanded = centroid + self.e_coeff*(reflected-centroid);
         end
+        
         function contracted = get_contracted(self,centroid)
+            % Contraction. Point between worst & centroid
             worst = self.simplex(:,end);
             contracted = centroid + self.c_coeff*(worst - centroid);
         end
+        
         function shrink_simplex(self)
             best = self.simplex(:,1);
             for i = 2:self.dimensions
@@ -108,5 +119,27 @@ classdef simplex_optimiser < handle
                 self.simplex(:,i) = current;
             end
         end
+        
+        function settings(self,varargin)
+            prop_array = [
+                self.init_size;
+                self.r_coeff;
+                self.e_coeff;
+                self.c_coeff;
+                self.s_coeff;
+                ];
+            idx = ~cellfun(@isempty,varargin);
+            for i = 1:nargin-1
+                if idx(i)
+                    prop_array(i) = varargin{i};
+                end
+            end
+            self.init_size = prop_array(1);
+            self.r_coeff = prop_array(2);
+            self.e_coeff = prop_array(3);
+            self.c_coeff = prop_array(4);
+            self.s_coeff = prop_array(5);
+        end
+        
     end
 end
